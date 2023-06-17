f5b49e9821a23c9a55fd828980e28b670dced369
kingssik <hyunsikkang25@gmail.com>
2023-06-17 09:49:21 +0900

    조회수 기능 구현 및 리스트 자릿수 정리

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -48,10 +48,10 @@ public class Main {
 
 				}
 
-				System.out.println("번호	| 제목");
+				System.out.println("번호	| 제목	| 조회수");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("%d | %s\n", article.id, article.title);
+					System.out.printf("%4d 	| %4s	| 	%2d\n", article.id, article.title, article.viewCnt);
 				}
 
 			} else if (cmd.startsWith("article detail")) {
@@ -74,10 +74,13 @@ public class Main {
 					continue;
 				}
 
+				foundArticle.increaseViewCnt();
+
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
+				System.out.printf("조회수 : %s\n", foundArticle.viewCnt);
 
 			} else if (cmd.startsWith("article modify")) {
 				String[] cmdBits = cmd.split(" ");
@@ -106,7 +109,7 @@ public class Main {
 
 				foundArticle.title = title;
 				foundArticle.body = body;
-				
+
 				System.out.printf("%d번 게시물이 수정되었습니다\n", id);
 
 			} else if (cmd.startsWith("article delete")) {
@@ -152,11 +155,17 @@ class Article {
 	String regDate;
 	String title;
 	String body;
+	int viewCnt;
 
 	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		this.viewCnt = 0;
+	}
+
+	public void increaseViewCnt() {
+		viewCnt++;
 	}
 }
