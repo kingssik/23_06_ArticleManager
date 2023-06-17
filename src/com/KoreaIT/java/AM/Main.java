e10ba11fe45c3b457e29310c410d032dd8aadbf8
kingssik <hyunsikkang25@gmail.com>
2023-06-17 10:18:06 +0900

    테스트데이터 생성 및 글번호 중복 해결

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -5,13 +5,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	private static List<Article> articles;
+
+	static {
+		articles = new ArrayList<>();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 
-		Scanner sc = new Scanner(System.in);
-		int lastArticleId = 0;
+		makeTestData();
 
-		List<Article> articles = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
 
 		while (true) {
 			System.out.printf("명령어 ) ");
@@ -27,8 +32,7 @@ public class Main {
 			}
 
 			if (cmd.equals("article write")) {
-				int id = lastArticleId + 1;
-				lastArticleId = id;
+				int id = articles.size() + 1;
 
 				String regDate = Util.getNowDateStr();
 				System.out.printf("제목 : ");
@@ -148,6 +152,13 @@ public class Main {
 
 		System.out.println("== 프로그램 종료 ==");
 	}
+
+	private static void makeTestData() {
+		System.out.println("테스트를 위한 데이터를 생성합니다");
+		articles.add(new Article(1, Util.getNowDateStr(), "title1", "body1", 11));
+		articles.add(new Article(2, Util.getNowDateStr(), "title2", "body2", 22));
+		articles.add(new Article(3, Util.getNowDateStr(), "title3", "body3", 33));
+	}
 }
 
 class Article {
@@ -157,12 +168,16 @@ class Article {
 	String body;
 	int viewCnt;
 
-	public Article(int id, String regDate, String title, String body) {
+	public Article(int id, String regDate, String title, String body, int viewCnt) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
-		this.viewCnt = 0;
+		this.viewCnt = viewCnt;
+	}
+
+	public Article(int id, String regDate, String title, String body) {
+		this(id, regDate, title, body, 0);
 	}
 
 	public void increaseViewCnt() {
